d0084270d8f0107eb929aab23e625e8cc94499fe
Marvin Roldan <marvinroldan_@hotmail.com>
2020-02-12 12:49:02 -0700

    Cleaned up certain areas

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace C_Review
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 35;
+            Console.WriteLine("Hello World!" + x);
+
+            if ( x < 30)
+            {
+                Console.WriteLine("X is less than thirty!");
+            }
+            else if (x == 30) 
+            {
+                Console.WriteLine("X is thirty!");
+            }
+            else
+            {
+                Console.WriteLine("X is greater than thirty!");
+            }
+
+            int a = 3;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Value is 1!");
+                    break;
+                case 2: 
+                    Console.WriteLine("Value is 2!");
+                    break;
+                case 3:
+                    Console.WriteLine("Value is 3!");
+                    break;
+                default:
+                    Console.WriteLine("Uh oh, it is another value!");
+                    break;
+            }
         }
     }
 }
